2db891fc06f97658283163e781c68c2339e1b72d
Kristin Vaughn <vaughn.kristin.n@gmail.com>
2021-01-06 15:38:17 -0500

    created addItem function to add todos

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,3 +1,6 @@
 app.controller('MainController', ['$scope', function($scope) {
     $scope.list = ['clean', 'store', 'study'];
+    $scope.addItem = function() {
+        $scope.list.push($scope.addToDo);
+    };
 }]);
