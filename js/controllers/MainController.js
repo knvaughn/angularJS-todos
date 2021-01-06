11b758b9be97674b74b7d80c49ee82b663b39eb5
Kristin Vaughn <vaughn.kristin.n@gmail.com>
2021-01-06 16:20:09 -0500

    created singlelist directive for todos and books lists

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,8 +1,10 @@
 app.controller('MainController', ['$scope', function($scope) {
     $scope.todo = {
+        title: "Things I need to do:",
         list: ['clean', 'store', 'study']
     };
     $scope.books = {
+        title: "Books I need to buy:",
         list: ['test']
     };
     $scope.addItem = function(itemList, item) {
