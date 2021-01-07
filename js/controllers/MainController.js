a1b2d60d0eece551e3ba382f0b35ebeaca566cbd
Kristin Vaughn <vaughn.kristin.n@gmail.com>
2021-01-07 10:33:25 -0500

    connected to google books api to allow adding books to list via ISBN number

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,13 +1,27 @@
-app.controller('MainController', ['$scope', function($scope) {
+app.controller('MainController', ['$scope', '$http', function($scope, $http) {
     $scope.todo = {
         title: "Things I need to do:",
         list: ['clean', 'store', 'study']
     };
     $scope.books = {
         title: "Books I need to buy:",
-        list: ['test']
+        list: []
     };
     $scope.addItem = function(itemList, item) {
-        itemList.push(item);
+        // ISBN: 10 or 13 in length and consist of only numbers
+        if((item.length === 10 || item.length === 13) && (/^[0-9]+$/).test(item)) {
+            console.log("ISBN");
+            $http.get(`https://www.googleapis.com/books/v1/volumes?q=isbn:${item}`)
+                .then(function(response) {
+                    itemList.push(`Title: ${response.data.items[0].volumeInfo.title} // Author(s): ${response.data.items[0].volumeInfo.authors}`);
+                }, function(error) {
+                    console.log(error);
+                });
+        } else {
+            console.log("Not an ISBN");
+            itemList.push(item);
+        }
+        
+        
     };
 }]);
